cc53cd5f1cc93ce0940e8d4905bc4a0893104b9f
Priscila Melo <melo.priscilatais@gmail.com>
2023-01-22 17:33:01 -0300

    add vercel

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.use("/tasks", tasksRoutes);
 app.use("/user", userRoutes);
 
